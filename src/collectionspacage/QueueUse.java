dd4d71f4c1d0ad8589ff1ff06e2167e53bbba28f
champaksworld <champaksworld@users.noreply.github.com>
2021-09-10 12:13:05 +0530

    Stack Use

---
+++ src/collectionspacage/QueueUse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collectionspacage;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+
+public class QueueUse {
+    public static void main(String[] args) {
+        Queue<String> queue=new ArrayDeque<>();
+        queue.add("1");
+        queue.add("2");
+        queue.add("3");
+        queue.forEach(s->{
+            System.out.println(s);});
+    }
+}
