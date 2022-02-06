e42cccfa5394eb8a05cc124352adf4163496e534
Champak's World <champaksworld@gmail.com>
2022-02-06 16:08:47 +0530

    Create QueueDemo.java

---
+++ src/collectionspackage/QueueDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collectionspackage;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<Integer> queue=new ArrayDeque<>();
+       
+    }
+  
+}
