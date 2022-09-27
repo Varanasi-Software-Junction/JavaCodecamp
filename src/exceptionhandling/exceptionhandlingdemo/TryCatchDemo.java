3b72c279d9efba3548b00889e73c1dce532f27e5
Champak's World <champaksworld@gmail.com>
2022-09-27 11:17:21 +0530

    Multiple catch exception

--- src/exceptionhandling/exceptionhandlingdemo/TryCatchDemo.java
+++ src/exceptionhandling/exceptionhandlingdemo/TryCatchDemo.java
@@ -8,11 +8,21 @@ public class TryCatchDemo {
 
     public static void main(String[] args) {
         try {
+            int[] a =null;// {9, 3, 4, 5};
+            a[0] = 9;
+            a[27] = 0;
             int n = 0;
             n = 1 / n;
             System.out.println(n);
 
+        } catch (NullPointerException ex) {
+            System.out.println("Null Pointer Exception");
+            System.out.println(ex);
+        } catch (ArithmeticException ex) {
+            System.out.println("Arithmetic Exception");
+            System.out.println(ex);
         } catch (Exception ex) {
+            System.out.println("Unknown Exception");
             System.out.println(ex);
 
         } finally {
