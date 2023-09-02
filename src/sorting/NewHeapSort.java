b1d5cc0c985dfe716c7c66cf540b97b09543a76f
champaksworldcreate <champaksworld@gmail.com>
2023-09-02 08:57:05 +0530

    Heap Sort Implemented

---
+++ src/sorting/NewHeapSort.java
@@ -0,0 +1,75 @@
+
+
+
+package sorting;
+
+public class NewHeapSort {
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Extract elements from the heap one by one
+        for (int i = n - 1; i > 0; i--) {
+            // Swap the root (maximum element) with the last element
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is an index in arr[]
+    private static void heapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // Left child
+        int right = 2 * i + 2; // Right child
+
+        // If left child is larger than root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // If right child is larger than largest so far
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Utility function to print an array
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 10, 3, 5, 1 };
+        int n = arr.length;
+
+        System.out.print("Unsorted Array: ");
+        printArray(arr);
+
+        heapSort(arr);
+
+        System.out.print("Sorted Array: ");
+        printArray(arr);
+    }
+}
