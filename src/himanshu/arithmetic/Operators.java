cd3d8e4945cea65ec6bcaed04d050e4e1f00f929
champaksworldcreate <champaksworld@gmail.com>
2023-12-06 10:50:36 +0530

    himanshu

---
+++ src/himanshu/arithmetic/Operators.java
@@ -0,0 +1,13 @@
+package himanshu.arithmetic;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int a = 20, b = 2;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
